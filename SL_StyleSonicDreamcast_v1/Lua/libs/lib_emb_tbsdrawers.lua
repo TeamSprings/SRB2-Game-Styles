5fb8f2f97dffebdf3847e982da92ab69738fb484
Sky Dusk <47698279+Ace-Lite@users.noreply.github.com>
2025-01-25 22:03:21 +0100

    ADV Font lib - additional function

--- SL_StyleSonicDreamcast_v1/Lua/libs/lib_emb_tbsdrawers.lua
+++ SL_StyleSonicDreamcast_v1/Lua/libs/lib_emb_tbsdrawers.lua
@@ -98,10 +98,44 @@ local function V_FontDrawer(v, font, x, y, scale, value, flags, color, alligment
 	end
 end
 
+local function V_FontUnadjustedDrawer(v, font, x, y, scale, value, flags, color, alligment, padding, leftadd, symbol)
+	if value == nil then return end
+	local str = value..""
+	local fontoffset = 0
+	local lenght = 0
+	local cache = {}
+
+	if leftadd then
+		str = strrep(symbol or ";", max(leftadd-#str, 0))..str
+	end
+
+	local maxv = #str
+
+	for i = 1,maxv do
+		local cur = V_CachePatches(v, patch, str, font, val, padding or 0, i)
+		cache[i] = cur
+		lenght = $+cur.width
+	end
+
+	if alligment == "center" then
+		x = $-(lenght*scale >> 1)
+	elseif alligment == "right" then
+		x = $-lenght*scale
+	end
+
+	local drawer = v.drawScaled
+
+	for i = 1,maxv do
+		drawer(x+fontoffset*scale, y, scale, cache[i].patch, flags, color)
+		fontoffset = $+cache[i].width
+	end
+end
+
+
 --TBSlib.fontlenghtcal(d, patch, str, font, val, padding, i)
 local function V_GetCharLenght(v, patch, str, font, val, padding, i)
 	local char = strsub(str, i, i)
 	return (fontregistry[font] and fontregistry[font][char] or V_RegisterFont(d, font, char)).width+padding
 end
 
-return {draw = V_FontDrawer, lenght = V_GetCharLenght}
+return {draw = V_FontDrawer, drawUdj = V_FontUnadjustedDrawer, lenght = V_GetCharLenght}
